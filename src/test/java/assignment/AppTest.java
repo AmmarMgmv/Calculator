234e2950c50b79d7478713a5f3843f711b06f914
AritroDe <dear@tcd.ie>
2022-10-09 14:09:34 +0100

    Update AppTest.java

--- src/test/java/assignment/AppTest.java
+++ src/test/java/assignment/AppTest.java
@@ -1,14 +1,18 @@
 package assignment;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-public class AppTest
-{
-    @Test
-    public void test(){
-        int expectedResult = 4;
-        assertEquals(2+2, expectedResult);
-    }
+public class AppTest {
+	
+	@Test
+	Public void tests() {
+		
+		Calculator testA = new Calculator("1+1");
+		assertEquals(2, testA.Calculate);
+		
+	}
+	
 }
