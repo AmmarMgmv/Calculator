4773c72f0e26251f4c92929875eccadcb3184748
AritroDe <dear@tcd.ie>
2022-10-10 22:33:07 +0100

    Finished all testing

--- src/test/java/assignment/AppTest.java
+++ src/test/java/assignment/AppTest.java
@@ -146,15 +146,24 @@ public class AppTest {
 
 	@Test
 	public void testingPrecedence() {
+		
 		boolean precedence1 = App.hasPrecedence('*', '-');
 		assertEquals(false, precedence1);
 
 		boolean precedence2 = App.hasPrecedence('+', '*');
 		assertEquals(true, precedence2);
+		
+		boolean precedence3 = App.hasPrecedence('-', '*');
+		assertEquals(true, precedence3);
+		
+		boolean precedence4 = App.hasPrecedence('*', '+');
+		assertEquals(false, precedence4);
+		
 	}
 
 	@Test
 	public void testingEvaluateExpression() {
+		
 		String input1 = App.evaluateExpression("12 423 + 413 - ");
 		String expected1 = "22";
 		assertEquals(expected1, input1);
@@ -162,5 +171,30 @@ public class AppTest {
 		String input2 = App.evaluateExpression("12 423 413 * + 7 - ");
 		String expected2 = "174704";
 		assertEquals(expected2, input2);
+		
+		String input3 = App.evaluateExpression("11 111 * 111 - 111111 + ");
+		String expected3 = "112221";
+		assertEquals(expected3, input3);
+		
+		String input4 = App.evaluateExpression("4412 31290 * 1143211 - ");
+		String expected4 = "136908269";
+		assertEquals(expected4, input4);
+		
+		String input5 = App.evaluateExpression("1213 9912 + 1288 109892 * - ");
+		String expected5 = "-141529771";
+		assertEquals(expected5, input5);
+		
+		String input6 = App.evaluateExpression("142 4133 - 214 4132 * + ");
+		String expected6 = "880257";
+		assertEquals(expected6, input6);
+		
+		String input7 = App.evaluateExpression("121411 3112 - 892 - 1898 11111 * + ");
+		String expected7 = "21206085";
+		assertEquals(expected7, input7);
+		
+		String input8 = App.evaluateExpression("979983 1123 1111 * - 1434344 + ");
+		String expected8 = "1166674";
+		assertEquals(expected8, input8);
+		
 	}
 } 
